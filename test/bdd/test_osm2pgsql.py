7d0ab9bef41678cfbdf0ee50f0b3e15d78e88b54
pocper1 <pocper1@gmail.com>
2026-03-11 14:25:00 +0800

    fix: python linting problems

--- test/bdd/test_osm2pgsql.py
+++ test/bdd/test_osm2pgsql.py
@@ -86,7 +86,8 @@ def update_from_osm_file(db_conn, def_config, osm2pgsql_options, opl_writer, doc
         The data is expected as attached text in OPL format.
     """
     create_table_triggers(db_conn, def_config)
-    asyncio.run(load_data(def_config.get_libpq_dsn(), 1), loop_factory=asyncio.WindowsSelectorEventLoopPolicy().new_event_loop)
+    from nominatim_db.utils.async_ops import run_legacy_asyncio
+    run_legacy_asyncio(load_data(def_config.get_libpq_dsn(), 1))
     cli.nominatim(['index'], def_config.environ)
     cli.nominatim(['refresh', '--functions'], def_config.environ)
 
--- test/bdd/utils/db.py
+++ test/bdd/utils/db.py
@@ -96,7 +96,8 @@ class DBManager:
             load_address_levels_from_config(conn, config)
             create_partition_tables(conn, config)
             create_functions(conn, config, enable_diff_updates=False)
-            asyncio.run(create_search_indices(conn, config), loop_factory=asyncio.WindowsSelectorEventLoopPolicy().new_event_loop)
+            from nominatim_db.utils.async_ops import run_legacy_asyncio
+            run_legacy_asyncio(create_search_indices(conn, config))
 
             tokenizer = tokenizer_factory.create_tokenizer(config)
             create_country_names(conn, tokenizer)
